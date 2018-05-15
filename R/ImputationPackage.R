488a2ce4859f64635568e74fc6e9fa6928cd795b
HannahVMeyer <hannah.v.meyer@googlemail.com>
2018-05-15 21:42:23 +0100

    Add imports

---
+++ R/ImputationPackage.R
@@ -0,0 +1,16 @@
+#' Imputation
+#'
+#' @docType package
+#' @name Imputation
+#' @importFrom stats median sd
+#' @importFrom utils read.table write.table 
+#' @importFrom dplyr filter
+#' @importFrom reshape2 melt acast
+#' @importFrom BaylorEdPsych LittleMCAR
+#' @importFrom Hmisc rcorr
+#' @import ggplot2
+#' @importFrom mice mice
+#' @importFrom VIM aggr
+#' @import phenix
+#' @import corrplot
+NULL
